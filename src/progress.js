e2aa281d723527f4a69887eced0a3539a7f848f0
Charles Lowell <cowboyd@frontside.io>
2015-11-19 12:39:49 -0600

    add tests for all progress events

---
+++ src/progress.js
@@ -0,0 +1,122 @@
+export default class Progress {
+  constructor(target, options = {observe: ()=> {}}) {
+    this.observe = options.observe;
+    this.target = target;
+    this.state = new State();
+    target.onloadstart = (event)=> {
+      this.update({loadStart: new ProgressEvent(event)});
+    };
+    target.onloadend = (event)=> {
+      this.update({loadEnd: new ProgressEvent(event)});
+    };
+    target.onprogress = (event)=> {
+      this.update((next)=> {
+        next.progress = next.progress.concat(new ProgressEvent(event));
+      });
+    };
+    target.onabort = (event)=> {
+      this.update({abort: new ProgressEvent(event)});
+    };
+    target.onerror = (event)=> {
+      this.update({error: new ProgressEvent(event)});
+    };
+    target.ontimeout = (event)=> {
+      this.update({timeout: new ProgressEvent(event)});
+    };
+  }
+  update(change) {
+    this.state = new State(this.state, change);
+    this.observe(this.state);
+  }
+}
+
+class State {
+  constructor(previous = {}, change = ()=> {}) {
+    Object.assign(this, {
+      abort: null,
+      error: null,
+      loadStart: null,
+      loadEnd: null,
+      timeout: null,
+      progress: []
+    }, previous);
+    if (change.call) {
+      change(this);
+    } else {
+      Object.assign(this, change);
+    }
+    Object.freeze(this.progress);
+    Object.freeze(this);
+  }
+
+  get isAborted() {
+    return !!this.abort;
+  }
+
+  get isErrored() {
+    return !!this.error;
+  }
+
+  get isLoadStarted() {
+    return !!this.loadStart;
+  }
+
+  get isLoadEnded() {
+    return !!this.loadEnd;
+  }
+
+  get isTimedOut() {
+    return !!this.timeout;
+  }
+
+  get latest() {
+    let event = this.progress[this.progress.length - 1];
+    if (!event) {
+      event = { total: 0, loaded: 0, lengthComputable: false };
+    }
+    return event;
+  }
+
+  get isLengthComputable() {
+    return this.latest.lengthComputable;
+  }
+
+  get loaded() {
+    return this.latest.loaded;
+  }
+
+  get total() {
+    return this.latest.total;
+  }
+
+  get ratio() {
+    if (this.loaded === 0 && this.total === 0) {
+      return 0;
+    }
+    return this.loaded / this.total;
+  }
+
+  get percentage() {
+    return Math.floor(this.ratio * 100);
+  }
+}
+
+
+class ProgressEvent {
+  constructor(event) {
+    this.event = event;
+    this.timestamp = new Date();
+  }
+
+  get lengthComputable() {
+    return this.event.lengthComputable;
+  }
+
+  get total() {
+    return this.event.total;
+  }
+
+  get loaded() {
+    return this.event.loaded;
+  }
+}
--- src/x-request.js
+++ src/x-request.js
@@ -1,106 +1,68 @@
+import Progress from './progress';
+
 let Top = this;
 
-class Progress {
-  constructor(target, options = {observe: ()=> {}}) {
-    this.observe = options.observe;
-    this.target = target;
-    this.state = new State();
+export default class XRequest {
+  constructor(options = {XMLHttpRequest: Top.XMLHttpRequest}) {
+    let xhr = new options.XMLHttpRequest();
+    xhr.responseType = options.responseType;
+    xhr.timeout = options.timeout || 0;
+    xhr.withCredentials = options.withCredentials || false;
+    let upload = new Progress(xhr.upload, {
+      observe: (fact)=> {
+        this.update((next)=> {
+          next.upload = fact;
+        });
+      }
+    });
+    let download = new Progress(xhr, {
+      observe: (fact)=> {
+        this.update((next)=> {
+          next.download = fact;
+        });
+      }
+    });
+    this.observe = options.observe || function() {};
+    this.state = new State({
+      xhr: xhr,
+      upload: upload.state,
+      download: download.state
+    });
   }
+
+  open(method, url) {
+    this.state.xhr.open(method, url);
+  }
+
+  send(object) {
+    this.state.xhr.send(object);
+  }
+
   update(change) {
     this.state = new State(this.state, change);
     this.observe(this.state);
   }
 }
 
-export default class XRequest extends Progress {
-  constructor(options = {XMLHttpRequest: Top.XMLHttpRequest}) {
-    super(new options.XMLHttpRequest(), options);
-    let upload = new Progress(this.target.upload, {
-      observe: (state)=> {
-        this.update((next)=> {
-          next.upload = state;
-        });
-      }
-    });
-    this.state = new State({
-      xhr: this.target,
-      upload: upload.state
-    });
-  }
-}
-
 
 class State {
   constructor(previous = {}, change = ()=> {}) {
-    Object.assign(this, {
-      abort: null,
-      error: null,
-      loadStart: null,
-      loadEnd: null,
-      timeout: null,
-      progress: []
-    }, previous);
+    Object.assign(this, previous, {
+      readyState: previous.xhr.readyState,
+      status: previous.xhr.status,
+      response: previous.xhr.response,
+      responseText: previous.xhr.responseText,
+      responseXML: previous.xhr.responseXML
+    });
     if (change.call) {
       change(this);
     } else {
       Object.assign(this, change);
     }
-    Object.freeze(this.progress);
-    Object.freeze(this);
   }
-
-  get isAborted() {
-    return !!this.abort;
-  }
-
-  get isErrored() {
-    return !!this.error;
-  }
-
-  get isLoadStarted() {
-    return !!this.loadStarted;
-  }
-
-  get isLoadEnded() {
-    return !!this.loadEnded;
-  }
-
-  get isTimedOut() {
-    return !!this.timeout;
-  }
-
-  get latest() {
-
-  }
-
-  get latest() {
-    let event = this.progress[this.progress.length - 1];
-    if (!event) {
-      event = { total: 0, loaded: 0, lengthComputable: false };
-    }
-    return event;
-  }
-
-  get isLengthComputable() {
-    return this.latest.lengthComputable;
-  }
-
-  get loaded() {
-    return this.latest.loaded;
-  }
-
-  get total() {
-    return this.latest.total;
-  }
-
-  get ratio() {
-    if (this.loaded === 0 && this.total ===0) {
-      return 0;
-    }
-    return this.loaded / this.total;
-  }
-
-  get percentage() {
-    return Math.floor(this.ratio * 100);
-  }
+  get isLoadStarted() { return this.download.isLoadStarted; }
+  get isLoadEnded() { return this.download.isLoadEnded; }
+  get isAborted() { return this.download.isAborted; }
+  get isErrored() { return this.download.isErrored; }
+  get isTimedOut() { return this.download.isTimedOut; }
 }
